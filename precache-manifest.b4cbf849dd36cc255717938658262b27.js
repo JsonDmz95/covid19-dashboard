cc5c8753f4b6cde4878e5213bd829a009f4e9c5e
Jeeson Domiguez <dominguez_js@outlook.com>
2020-08-05 10:32:16 -0600

    Updates

---
+++ precache-manifest.b4cbf849dd36cc255717938658262b27.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d2dcab659dd4dc0d3375adfd68117ad",
+    "url": "/covid19-dashboard/index.html"
+  },
+  {
+    "revision": "074a98a94155c90aa56f",
+    "url": "/covid19-dashboard/static/css/main.6c3c4c4d.chunk.css"
+  },
+  {
+    "revision": "9ec538cb7b28ed7ef23e",
+    "url": "/covid19-dashboard/static/js/2.f64f6548.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19-dashboard/static/js/2.f64f6548.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "074a98a94155c90aa56f",
+    "url": "/covid19-dashboard/static/js/main.1dea8bce.chunk.js"
+  },
+  {
+    "revision": "debad3871e3e806aac61",
+    "url": "/covid19-dashboard/static/js/runtime-main.6c31e1de.js"
+  },
+  {
+    "revision": "84423651c92e354c4bd63d2b8351490d",
+    "url": "/covid19-dashboard/static/media/arrow.84423651.svg"
+  },
+  {
+    "revision": "406df75f7e6eabe35d1880184f498cc3",
+    "url": "/covid19-dashboard/static/media/holder.406df75f.png"
+  },
+  {
+    "revision": "20aa21f1076485fb26828ff66a789115",
+    "url": "/covid19-dashboard/static/media/link.20aa21f1.png"
+  },
+  {
+    "revision": "129e88d5b1008fc268514a76cedbc1a4",
+    "url": "/covid19-dashboard/static/media/logo.129e88d5.svg"
+  },
+  {
+    "revision": "5a5ed9addeb7f9ca8eaf5f53b9e931cb",
+    "url": "/covid19-dashboard/static/media/overview.5a5ed9ad.svg"
+  },
+  {
+    "revision": "3939a4f6ceaaf59a525de7488f4cd325",
+    "url": "/covid19-dashboard/static/media/search.3939a4f6.svg"
+  }
+]);
